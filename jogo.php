778cc42159e2318f1047778f8e4d67b038a02865
Etec <etec@etec.com>
2023-05-09 17:22:55 -0300

    Adição dos arquivos já existentes

---
+++ jogo.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<html>
+<head>
+    <meta charset="utf-8">
+
+    <title>Pife!</title>
+		<link rel="stylesheet" type="text/css" href="resources/css.css" />
+		<link rel="icon" type="image/png" href="resources/fav1.png">
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<script src="resources/favicon.js"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="resources/style.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body id="areaJogo">
+ 
+    <nav id="nav-lateral">
+        <ul id="itens-navlateral">
+            <li>
+                <a href="index.php" id="logo-navlateral" class="links-navlateral">
+                    <img src="resources/logo.png" alt="logo">
+                    <span class="nav-item">Pife!</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="links-navlateral">
+                    <i class="fas fa-home"></i>
+                    <span class="nav-item">Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="links-navlateral">
+                    <i class="fas fa-user"></i>
+                    <span class="nav-item">Perfil</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="links-navlateral">
+                    <i class="fas fa-tasks"></i>
+                    <span class="nav-item">Tarefas</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="links-navlateral">
+                    <i class="fas fa-trophy"></i>
+                    <span class="nav-item">Ranking</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="links-navlateral">
+                    <i class="fas fa-question-circle"></i>
+                    <span class="nav-item">FAQ</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="links-navlateral">
+                    <i class="fas fa-headset"></i>
+                    <span class="nav-item">Suporte</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="links-navlateral">
+                    <i class="fas fa-cog"></i>
+                    <span class="nav-item">Configurações</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" id="logout" class="links-navlateral">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span class="nav-item">Log Out</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+</body>
+</html>
